013176c0d69bdb5149ed928c2ef79503be13f6ff
Marcin Misiaszek <marcin.misiaszek@gmail.com>
2026-03-04 10:27:19 +0000

    bitset.cpp modified

--- cw1/bitset.cpp
+++ cw1/bitset.cpp
@@ -1,18 +1,25 @@
-#include <iostream>
-#include <bitset>
+#include <stdio.h>
+#include <limits.h>
 
-using namespace std;
+void print_bits(unsigned short value) {
+    for (int i = 15; i >= 0; i--) {
+        printf("%d", (value >> i) & 1);
+    }
+    printf("\n");
+}
 
 int main() {
     unsigned short short1 = 4;
-    bitset<16> bitset1{short1};   // the bitset representation of 4
-    cout << bitset1 << endl;  // 0b00000000'00000100
+    printf("bitset1: ");
+    print_bits(short1);
 
-    unsigned short short2 = short1 << 1;     // 4 left-shifted by 1 = 8
-    bitset<16> bitset2{short2};
-    cout << bitset2 << endl;  // 0b00000000'00001000
+    unsigned short short2 = short1 << 1;
+    printf("bitset2: ");
+    print_bits(short2);
 
-    unsigned short short3 = short1 << 2;     // 4 left-shifted by 2 = 16
-    bitset<16> bitset3{short3};
-    cout << bitset3 << endl;  // 0b00000000'00010000
+    unsigned short short3 = short1 << 2;
+    printf("bitset3: ");
+    print_bits(short3);
+
+    return 0;
 }
