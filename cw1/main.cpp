e9144d883ac53d3630196d12bc1394bfc95e7a5e
Marcin Misiaszek <marcin.misiaszek@gmail.com>
2026-03-04 10:37:09 +0000

    main.cpp changed

--- cw1/main.cpp
+++ cw1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "hello world" << std::endl;
+    std::cout.precision(2);
+    std::cout << "pi: " << 3.14159 << std::endl;
     return 0;
 }
