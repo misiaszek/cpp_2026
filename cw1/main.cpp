f8d8e4350ab991b3bf411725889da7c3b81b853a
Marcin Misiaszek <marcin.misiaszek@gmail.com>
2026-03-04 11:43:56 +0000

    std::operator<<(cout,sting) presented

--- cw1/main.cpp
+++ cw1/main.cpp
@@ -4,5 +4,10 @@ int main() {
     std::cout << "hello world" << std::endl;
     std::cout.precision(2);
     std::cout << "pi: " << 3.14159 << std::endl;
+    // std::cout - jest obiektem, operator<< jest funkcją, która przyjmuje dwa argumenty: 
+    // strumień wyjściowy (std::cout) i tekst do wypisania ("Hello world"). Funkcja ta zwraca 
+    // referencję do strumienia wyjściowego, co pozwala na łańcuchowe wywoływanie operatora<<. 
+    // W tym przypadku, po wypisaniu "Hello world", zostanie również wypisany znak nowej linii (std::endl).
+    (std::operator<<(std::cout, "Hello world")).operator<<(std::endl); 
     return 0;
 }
