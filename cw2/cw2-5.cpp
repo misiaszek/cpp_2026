a1318a2d643cb34d5a2b8cfdfe42b496e6149821
Marcin Misiaszek <marcin.misiaszek@gmail.com>
2026-03-11 08:46:10 +0000

    cw 4 i 5 added

---
+++ cw2/cw2-5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Employee // members are private by default
+{
+    std::string m_name{};
+    char m_firstInitial{};
+
+public:
+    void setName(std::string_view name)
+    {
+        m_name = name;
+        m_firstInitial = name.front(); // use std::string::front() to get first letter of `name`
+    }
+
+    void print() const
+    {
+        std::cout << "Employee " << m_name << " has first initial " << m_firstInitial << '\n';
+    }
+};
+
+int main()
+{
+    Employee e{};
+    e.setName("John");
+    e.print();
+
+    e.setName("Mark");
+    e.print();
+
+    return 0;
+}
