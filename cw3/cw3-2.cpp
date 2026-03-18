9ef2e3710aaafd3296ca5df04116f5ecffcad5db
Marcin Misiaszek <marcin.misiaszek@gmail.com>
2026-03-18 09:17:16 +0100

    Create cw3-2.cpp

---
+++ cw3/cw3-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Fraction
+{
+private:
+    int m_numerator{ 0 };
+    int m_denominator{ 1 };
+
+public:
+    // Default constructor
+    Fraction(int numerator=0, int denominator=1)
+        : m_numerator{numerator}, m_denominator{denominator}
+    {
+    }
+
+    void print() const
+    {
+        std::cout << "Fraction(" << m_numerator << ", " << m_denominator << ")\n";
+    }
+};
+
+int main()
+{
+    Fraction f { 5, 3 };  // Calls Fraction(int, int) constructor
+    Fraction fCopy { f }; // What constructor is used here?
+
+    f.print();
+    fCopy.print();
+
+    return 0;
+}
